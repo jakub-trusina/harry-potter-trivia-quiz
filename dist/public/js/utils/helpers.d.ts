085d6114cbfbc038a78c47ef4d9cd51db34d1c26
Jakub Trusina <jakub.trusina@datasentics.com>
2025-04-14 19:16:54 +0200

    removed unnecessary stuff

--- dist/public/js/utils/helpers.d.ts
+++
@@ -1,6 +0,0 @@
-import { House } from '../types/game';
-export declare function getHouseFromName(name: string): House | null;
-export declare function resetHouseAssignments(): void;
-export declare function formatResponseTime(ms: number): string;
-export declare function moveModalsToBody(): void;
-export declare function debugDuel(message: string): void;
